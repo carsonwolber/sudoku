bc1c4116ec6a016520959b082b558691c54d56cd
carsonwolber <ctw54@cornell.edu>
2024-12-18 04:42:51 -0500

    added gui

---
+++ src/Main.java
@@ -0,0 +1,25 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import javax.swing.*;
+
+public class Main {
+    private static void createAndShowGUI() {
+        JFrame frame = new JFrame("Carson's Sudoku Solver");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        JLabel label = new JLabel("Hello World");
+        frame.getContentPane().add(label);
+
+        frame.pack();
+        frame.setVisible(true);
+    }
+
+    public static void main(String[] args) {
+
+        javax.swing.SwingUtilities.invokeLater(new Runnable() {
+            public void run() {
+                createAndShowGUI();
+            }
+        });
+    }
+}
