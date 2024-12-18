fb7615b75f72b8378795065c3aacd2ab5b27edbf
carsonwolber <ctw54@cornell.edu>
2024-12-18 05:08:15 -0500

    added tile class

---
+++ src/Tile.java
@@ -0,0 +1,28 @@
+public class Tile {
+
+    private int value;
+    private boolean fixed;
+    private final int rowIndex;
+    private final int colIndex;
+
+    public Tile(int rowIndex, int colIndex) {
+        this.rowIndex = rowIndex;
+        this.colIndex = colIndex;
+        this.value = 0;
+        this.fixed = false;
+
+    }
+
+    public void setValue(int newValue){
+        if(!fixed && newValue > 0 && newValue < 10){
+            this.value = newValue;
+        }
+    }
+
+    public int findBoxIndex(int rowIndex, int colIndex) {
+        return (rowIndex /3) + (colIndex /3) *3;
+    }
+
+
+
+}
