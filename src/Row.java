5ce1e84cbcd83aebcf0b3e9e8642d8c273e72189
Carson Wolber <118689146+carsonwolber@users.noreply.github.com>
2024-12-27 05:32:36 -0500

    Create Row.java

---
+++ src/Row.java
@@ -0,0 +1,36 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Row {
+
+    private final int index;
+    private List<Tile> tiles;
+
+    public Row(int index) {
+        tiles = new ArrayList<>();
+        this.index = index;
+    }
+
+    public void addTile(Tile tile) {
+        tiles.add(tile);
+    }
+
+    public int getIndex() {
+        return this.index;
+    }
+
+    public boolean isValid() {
+        boolean[] seen = new boolean[10];
+        for(Tile tile : tiles) {
+            int value = tile.getValue();
+            if(seen[value] && value != 0){
+                return false;
+            }
+            else {
+                seen[value] = true;
+            }
+        }
+        return true;
+    }
+
+}
