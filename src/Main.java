4a5a8cfb06c9a0763b52c08b38503193edb558d3
carsonwolber <ctw54@cornell.edu>
2024-12-27 04:51:36 -0500

    added sudoku grid in jframe

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,29 @@
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 import javax.swing.*;
+import java.awt.*;
 
 public class Main {
     private static void createAndShowGUI() {
         JFrame frame = new JFrame("Carson's Sudoku Solver");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setLayout(null);
 
-        JLabel label = new JLabel("Hello World");
-        frame.getContentPane().add(label);
+        int size = 50;
+        int padding = 5;
+        JTextField[][] grid = new JTextField[9][9];
+        for (int i = 0; i < 9; i++) {
+            for (int j = 0; j < 9; j++) {
+                grid[i][j] = new JTextField();
+                grid[i][j].setBorder(BorderFactory.createLineBorder(Color.BLACK));
+
+                int x = i * (size +padding) + 10;
+                int y = j * (size + padding) + 10;
+                grid[i][j].setBounds(x, y, size, size);
+                frame.add(grid[i][j]);
+            }
+        }
+
 
         frame.pack();
         frame.setVisible(true);
