128d735563255c4dbd6671d1cf215f72ae0216c2
carsonwolber <ctw54@cornell.edu>
2024-12-28 08:18:25 -0500

    added box class

---
+++ src/Box.java
@@ -0,0 +1,42 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Box {
+
+    private List<Tile> tiles;
+    private final int index;
+
+    public Box(int index){
+        this.index = index;
+        tiles = new ArrayList<>();
+    }
+
+    public void addTile(Tile tile) {
+        tiles.add(tile);
+    }
+
+    public List<Tile> getTiles() {
+        return tiles;
+    }
+
+    public int getIndex() {
+        return this.index;
+    }
+
+    public boolean isValid() {
+        boolean[] seen = new boolean[10];
+        for(Tile tile : tiles) {
+            int value = tile.getValue();
+            if(value < 0 || value > 9) {
+                return false;
+            }
+            if(seen[value] && value != 0){
+                return false;
+            }
+            else {
+                seen[value] = true;
+            }
+        }
+        return true;
+    }
+}
